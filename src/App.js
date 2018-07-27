de12d828fd140a81918f9360c19dca26f1762a81
Heathcliff <dennis.nathan@gmail.com>
2018-07-27 12:10:38 -0600

    Initial commit after installing material-ui

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import Button from '@material-ui/core/Button';
+
+class App extends Component {
+
+  render() {
+    return (
+    <Button variant="raised" color="primary">
+      Hello World
+    </Button>
+  );
+  }
+}
+
+export default App;
