16782d5b6ed1e186d608a6660499876d6b723261
Heathcliff <dennis.nathan@gmail.com>
2018-07-27 12:21:24 -0600

    Added toolbar component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import Button from '@material-ui/core/Button';
+// import Button from '@material-ui/core/Button';
+
+import Toolbar from './Toolbar/Toolbar';
 
 class App extends Component {
 
   render() {
     return (
-    <Button variant="raised" color="primary">
-      Hello World
-    </Button>
+      <Toolbar />
   );
   }
 }
---
+++ src/Toolbar/Toolbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const toolbar = () => (
+  <React.Fragment>
+    <AppBar position="static">
+      <Toolbar>
+        {/* <IconButton color="inherit" aria-label="Menu">
+          <MenuIcon />
+        </IconButton> */}
+        <Typography variant="title" color="inherit">
+          News
+        </Typography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
+  </React.Fragment>
+)
+
+export default toolbar;
